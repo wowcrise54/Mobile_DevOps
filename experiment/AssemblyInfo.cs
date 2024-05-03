8924dbe50e3ffac3a913ad816f328ed97bd4d140
kasat1k89 <liulenoff@yandex.ru>
2024-05-04 01:21:49 +0500

    test message

---
+++ experiment/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("inglobal.ttf", Alias ="inglobal" )]
+[assembly: ExportFont("inglobalb.ttf", Alias = "inglobalbold")]
---
+++ experiment/ProfilePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace experiment
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
