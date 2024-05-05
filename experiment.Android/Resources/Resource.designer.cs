051ce6d8c6087d04e29b0559f78c992dfe8a5f74
Лёха <@DESKTOP-6SJSGEG>
2024-05-05 21:06:14 +0500

    Сделал кликабельными картинки + перенаправление на старницы

--- experiment.Android/Resources/Resource.designer.cs
+++ experiment.Android/Resources/Resource.designer.cs
@@ -16694,16 +16694,19 @@ namespace experiment.Droid
 			public const int settings = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int test_custom_background = 2131165338;
+			public const int splash_screen = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int tooltip_frame_dark = 2131165339;
+			public const int test_custom_background = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int tooltip_frame_light = 2131165340;
+			public const int tooltip_frame_dark = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int zozh = 2131165341;
+			public const int tooltip_frame_light = 2131165341;
+			
+			// aapt resource value: 0x7F07009E
+			public const int zozh = 2131165342;
 			
 			static Drawable()
 			{
