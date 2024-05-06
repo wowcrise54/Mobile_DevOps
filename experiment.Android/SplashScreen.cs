1e8b6c0d2531832cfd90cad4209c5e3e82d05fea
Лёха <@DESKTOP-6SJSGEG>
2024-05-06 20:45:28 +0500

    Сделал загрузочный экран при входе в приложение

---
+++ experiment.Android/SplashScreen.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace experiment.Droid
+{
+    [Activity(Label = "experiment", Icon = "@mipmap/iconour", Theme = "@style/Thema", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize)]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+
+            // Create your application here
+        }
+    }
+}
