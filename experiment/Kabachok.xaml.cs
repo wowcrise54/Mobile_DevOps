28e0bd72d2106ea7cead7b5e2b83261be34dfd99
Лёха <@DESKTOP-6SJSGEG>
2024-05-10 01:34:14 +0500

    Добавил обработчик для таймера и кнопки избранное

--- experiment/Kabachok.xaml.cs
+++ experiment/Kabachok.xaml.cs
@@ -21,5 +21,66 @@ namespace experiment
         {
             await Navigation.PushAsync(new ProfilePage());
         }
+
+        private void OnClick(object sender, EventArgs e)
+        {
+            var image = sender as Image;
+            if (image == null) return;
+
+           
+            if (Application.Current.UserAppTheme == OSAppTheme.Dark)
+            {
+                if (image.Source.ToString().Contains("dark_not_like.png"))
+                    image.Source = "dark_like.png"; 
+                else
+                    image.Source = "dark_not_like.png"; 
+            }
+            else
+            {
+                if (image.Source.ToString().Contains("light_not_like.png"))
+                    image.Source = "light_like.png";
+                else
+                    image.Source = "light_not_like.png"; 
+            }
+        }
+        private System.Timers.Timer _timer;
+        private TimeSpan _timeLeft;
+        private void OnTime(object sender, EventArgs e)
+        {
+            if (_timer != null && _timer.Enabled)
+                return; 
+
+           
+            string timeText = timeLabel.Text.Replace(" мин", ""); 
+            if (TimeSpan.TryParse(timeText, out _timeLeft))
+            {
+                _timer = new System.Timers.Timer(1000); 
+                _timer.Elapsed += OnTimerElapsed;
+                _timer.Start();
+            }
+        }
+       
+  
+        private void OnTimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _timeLeft = _timeLeft.Subtract(TimeSpan.FromSeconds(1));
+            if (_timeLeft.TotalSeconds <= 0)
+            {
+                _timer.Stop();
+                _timer.Dispose();
+                _timer = null;
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    timeLabel.Text = "00:00 мин";
+                });
+            }
+            else
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    timeLabel.Text = $"{_timeLeft:mm\\:ss} мин";
+                });
+            }
+        }
     }
 }
--- experiment/MainPage.xaml.cs
+++ experiment/MainPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,8 +15,52 @@ namespace experiment
 		public MainPage ()
 		{
 			InitializeComponent ();
-         
-            
+            Application.Current.RequestedThemeChanged += OnThemeChanged;
+
         }
-	}
+        protected override void OnDisappearing()
+        {
+            Application.Current.RequestedThemeChanged -= OnThemeChanged;
+            base.OnDisappearing();
+        }
+        private void OnThemeChanged(object sender, AppThemeChangedEventArgs e)
+        {
+            ResetImagesToDefault();
+        }
+
+        private void ResetImagesToDefault()
+        {
+         
+            foreach (var image in FindImages(this))
+            {
+                if (Application.Current.UserAppTheme == OSAppTheme.Dark ||
+                    Application.Current.UserAppTheme == OSAppTheme.Unspecified && AppInfo.RequestedTheme == AppTheme.Dark)
+                {
+                    image.Source = "dark_not_like.png";
+                }
+                else
+                {
+                    image.Source = "light_not_like.png";
+                }
+            }
+        }
+
+        private IEnumerable<Image> FindImages(VisualElement parent)
+        {
+            if (parent is Image image)
+            {
+                yield return image;
+            }
+            else if (parent is Layout<View> layout)
+            {
+                foreach (var child in layout.Children)
+                {
+                    foreach (var childImage in FindImages(child))
+                    {
+                        yield return childImage;
+                    }
+                }
+            }
+        }
+    }
 }
