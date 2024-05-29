0bcddfa0fecbdbbf79d5f9b0b8d14e09b2225ebe
Анатолий Портнягин <tolikportnyagin@yandex.ru>
2024-05-29 23:43:02 +0500

    Небольшие правки

--- backend/profile/routes.py
+++ backend/profile/routes.py
@@ -23,11 +23,8 @@ def get_profile():
 @jwt_required()
 def edit_profile():
     jwt_identity = get_jwt_identity()
-    print(f"jwt_identity: {jwt_identity}")
     id = jwt_identity.get('id')
-    print(f"id: {id}")
     profile = Profile.query.filter_by(id=id).first()
-    print (f"profile: {profile}")
     
     if request.method == 'GET':
         if not profile:
@@ -40,22 +37,9 @@ def edit_profile():
 
     if request.method == 'POST':
         data = request.get_json()
-        print(f"Received data: {data}")
-
         profile.name = data.get('name')
         profile.password = data.get('pass')
         profile.mail = data.get('mail')
-
-        profile_data = {
-            "name": profile.name,
-            "mail": profile.mail,
-            "pass": profile.password
-        }
-        print(f"Profile data before commit: {profile_data}")
-
         db.session.commit()
-
-        print("Profile updated in database")
-
         return jsonify({"msg": "Profile updated"
         }), 200
